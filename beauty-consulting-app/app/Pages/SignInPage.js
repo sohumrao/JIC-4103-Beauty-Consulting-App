73fb830ce75e20230da981efb3d5fc271e856a33
achen443 <achen443@gatech.edu>
2024-09-20 17:13:00 -0400

    Added globla styles sheet and updated the Signup Page with it

--- beauty-consulting-app/app/Pages/SignInPage.js
+++ beauty-consulting-app/app/Pages/SignInPage.js
@@ -1,9 +1,10 @@
 import React, { useContext, useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import { Link, useNavigation } from '@react-navigation/native';
+import { View, TextInput, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
 import { UserContext } from '../App';
 import SignupBackground from '../assets/components/SignupBackground';
+import globalStyles from '../GlobalStyles';
 
 const SignInPage = () => {
     const [username, setUsername] = useState('');
@@ -12,67 +13,14 @@ const SignInPage = () => {
 
     const userContext = useContext(UserContext);
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#f5f5f5',
-        },
-        box: {
-            width: '80%',
-            padding: 20,
-            backgroundColor: '#fff',
-            borderRadius: 10,
-            shadowColor: '#000',
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 5,
-        },
-        title: {
-            fontSize: 24,
-            fontWeight: 'bold',
-            marginBottom: 20,
-            textAlign: 'center',
-        },
-        input: {
-            height: 40,
-            borderColor: '#ccc',
-            borderWidth: 1,
-            borderRadius: 5,
-            paddingHorizontal: 10,
-            marginBottom: 15,
-        },
-        button: {
-            backgroundColor: '#FF5252',
-            paddingVertical: 10,
-            borderRadius: 5,
-        },
-        buttonText: {
-            color: '#fff',
-            textAlign: 'center',
-            fontWeight: 'bold',
-        },
-        needAccountText: {
-            textAlign: 'center',
-            marginTop: 20
-        },
-        createAccountLink: {
-            textAlign: 'center',
-            marginTop: 10,
-            color: '#FF5252'
-        }
-    });
-
     const handleSignIn = async () => {
-        req = {
-                username: username,
-                password: password
+        const req = {
+            username: username,
+            password: password
         };
         console.log(req.body);
         try {
-            apiUrl = process.env.EXPO_PUBLIC_API_URL;
+            const apiUrl = process.env.EXPO_PUBLIC_API_URL;
             if (!apiUrl) {
                 console.error("API URL not defined");
                 return;
@@ -100,10 +48,10 @@ const SignInPage = () => {
 
     return (
         <SignupBackground>
-            <View style={styles.box}>
-                <Text style={styles.title}>Sign In</Text>
+            <View style={globalStyles.box}>
+                <Text style={globalStyles.title}>Sign In</Text>
                 <TextInput
-                    style={styles.input}
+                    style={globalStyles.input}
                     placeholder="Username"
                     value={username}
                     onChangeText={setUsername}
@@ -111,19 +59,19 @@ const SignInPage = () => {
                     autoCapitalize="none"
                 />
                 <TextInput
-                    style={styles.input}
+                    style={globalStyles.input}
                     placeholder="Password"
                     value={password}
                     onChangeText={setPassword}
                     secureTextEntry
                     autoCapitalize="none"
                 />
-                <TouchableOpacity style={styles.button} onPress={handleSignIn}>
-                    <Text style={styles.buttonText}>Sign In</Text>
+                <TouchableOpacity style={globalStyles.button} onPress={handleSignIn}>
+                    <Text style={globalStyles.buttonText}>Sign In</Text>
                 </TouchableOpacity>
-                <Text style={styles.needAccountText}>Dont have an account?</Text>
-                <Text style={styles.createAccountLink} onPress={() => {navigation.navigate("Create Account") }}>Create one.</Text>
-                <Text style={styles.createAccountLink} onPress={() => {navigation.navigate("Forgot Password") }}>Forgot Password?</Text>
+                <Text style={globalStyles.promptText}>Don't have an account?</Text>
+                <Text style={globalStyles.linkText} onPress={() => { navigation.navigate("Create Account") }}>Create one.</Text>
+                <Text style={globalStyles.linkText} onPress={() => { navigation.navigate("Forgot Password") }}>Forgot Password?</Text>
             </View>
         </SignupBackground>
     );
