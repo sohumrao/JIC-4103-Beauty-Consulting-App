15a7dd970b6207fab81c7c307d87c7572b777073
achen443 <achen443@gatech.edu>
2024-09-24 15:17:31 -0400

    Fixed the stlying bug on the landing page

--- beauty-consulting-app/app/Pages/LandingPage.js
+++ beauty-consulting-app/app/Pages/LandingPage.js
@@ -1,29 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useNavigation } from '@react-navigation/native';
-import globalStyles from '../assets/GlobalStyles';
+import { Image } from 'react-native';
 
+import ClientDetails from './ClientDetails';
 import ContinueButton from '../assets/components/ContinueButton';
 import LandingPageImg from '../assets/images/LandingPageImg.png';
 import SignupBackground from '../assets/components/SignupBackground';
 
+
 function LandingPage() {
   const navigation = useNavigation();
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+    },
+    imageContainer: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 50,
+      marginBottom: -50,
+    },
+    circleImage: {
+      width: 350,
+      height: 350,
+      borderRadius: 175,
+      borderWidth: 3,
+      borderColor: 'black',
+      overflow: 'hidden',
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      marginTop: -48, 
+    },
+    buttonContainer: {
+      width: '100%',
+      paddingHorizontal: 20,
+      paddingVertical: 20,
+    },
+  });
+
   return (
     <SignupBackground>
-      <View style={globalStyles.box}>
-        <View style={globalStyles.imageContainer}>
-          <Image 
-            source={LandingPageImg} 
-            style={globalStyles.circleImage}
-          />
-        </View>
-        <Text style={globalStyles.title}>Sign up as a client</Text>
-        <View style={globalStyles.buttonContainer}>
-          <ContinueButton onPress={() => navigation.navigate('ClientDetails')} />
-        </View>
+      <View style={styles.imageContainer}>
+        <Image 
+          source={LandingPageImg} 
+          style={styles.circleImage}
+        />
+      </View>
+      <Text style={styles.title}>Sign up as a client</Text>
+      <View style={styles.buttonContainer}>
+        <ContinueButton onPress={() => navigation.navigate('ClientDetails')} />
       </View>
     </SignupBackground>
   );
---
+++ beauty-consulting-app/app/assets/components/ProfileImage.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { View, Button, Image, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+const ProfileImage = () => {
+  const [imageUri, setImageUri] = useState(null);
+
+  const pickImage = async () => {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      Alert.alert("Error");
+      return;
+    }
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImageUri(result.assets[0].uri);
+    }
+  };
+//to do: send imageUri to backend
+  return (
+    <View style={{ alignItems: 'center', justifyContent: 'center'}}>
+      <Button title="Picture Test" onPress={pickImage} />
+      {imageUri && <Image source={{ uri: imageUri }} style={{ width: 200, height: 200, borderRadius: 100, marginTop: 20 }} />}
+    </View>
+  );
+};
+
+export default ProfileImage;
