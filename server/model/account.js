39c3c7d796f7fe2a10eb99447596feceab4a1a2f
willshutt <willmshutt@gmail.com>
2024-09-24 16:26:49 -0400

    add hashing

--- server/model/account.js
+++ server/model/account.js
@@ -1,14 +1,30 @@
 import mongoose from "mongoose";
+import crypto from "crypto";
 
 const AccountSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true
     },
-    password: {
-        type: String,
-    }
+    hash: String,
+    salt: String
 });
+
+AccountSchema.methods.createHashedPassword = function (password) {
+    // Creating a unique salt for a particular user
+    this.salt = crypto.randomBytes(16).toString('hex');
+ 
+    // Hashing user's salt and password with 1000 iterations,
+    // 64 length and sha512 digest
+    this.hash = crypto.pbkdf2Sync(password, this.salt,
+        1000, 64, `sha512`).toString(`hex`);
+}
+
+AccountSchema.methods.validateHashedPassword = function (password) {
+    var hash = crypto.pbkdf2Sync(password,
+        this.salt, 1000, 64, `sha512`).toString(`hex`);
+    return this.hash === hash;
+}
 
 const ResetPasswordSchema = new mongoose.Schema({
     email: {
