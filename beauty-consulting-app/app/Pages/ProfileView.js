750587cb22503c43d36522710c9f40328db60f96
clark <ccousins7@gatech.edu>
2024-09-29 18:45:03 -0400

    Added backend functionality to edit/deleteProfile

--- beauty-consulting-app/app/Pages/ProfileView.js
+++ beauty-consulting-app/app/Pages/ProfileView.js
@@ -1,144 +1,157 @@
-import React, { useContext, useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Input} from 'react-native';
-import { Link, useNavigation } from '@react-navigation/native';
-import { UserContext } from '../App';
-import SignupBackground  from '../assets/components/SignupBackground';
-import globalStyles from '../assets/GlobalStyles';
-import KeyboardAvoidingInput from '../assets/components/KeyboardAvoidingInput';
-import  axios  from 'axios';
+import React, { useContext, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Link, useNavigation } from "@react-navigation/native";
+import { UserContext } from "../App";
+import SignupBackground from "../assets/components/SignupBackground";
+import globalStyles from "../assets/GlobalStyles";
+import KeyboardAvoidingInput from "../assets/components/KeyboardAvoidingInput";
+import axios from "axios";
 
 const ProfileView = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    var userContext = useContext(UserContext);
-    var [name ,setName] = useState('');
-    var [age, setAge] = useState('');
-    var [gender, setGender] = useState('');
-    var [phoneNumber, setPhoneNumber] = useState('');
-    var [allergies, setAllergies] = useState('');
-    var [concerns, setConcerns] = useState('');
-    var [isEdit, setIsEdit] = useState(false);
+  var userContext = useContext(UserContext);
+  var [name, setName] = useState("");
+  var [age, setAge] = useState("");
+  var [gender, setGender] = useState("");
+  var [phoneNumber, setPhoneNumber] = useState("");
+  var [allergies, setAllergies] = useState("");
+  var [concerns, setConcerns] = useState("");
+  var [isEdit, setIsEdit] = useState(false);
 
-    const handleEdit = async () => {
-        if (isEdit) {
-            req = {
-                username: userContext.username,
-                name: name,
-                age: age,
-                gender: gender,
-                phoneNumber: phoneNumber,
-                email: userContext.email,
-                hairDetails: userContext.hairDetails,
-                allergies: allergies,
-                concerns: concerns,
-            };
-            try {
-                const apiURL = process.env.EXPO_PUBLIC_API_URL + ':5050/client/' + userContext.username;
-                if (!apiURL) {
-                    console.error("apiURL not defined");
-                    return;
-                } 
-                const res = await axios.put(apiURL, req);
-                console.log("update successful: ", res.data);
-            } catch (error) {
-                console.error("Error with request: ", error);
-            }
-
-            // update user context for rest of session
-            userContext = userContext.updateUserContext(req)
+  const handleEdit = async () => {
+    if (isEdit) {
+      req = {
+        username: userContext.username,
+        name: name,
+        age: age,
+        gender: gender,
+        phoneNumber: phoneNumber,
+        email: userContext.email,
+        hairDetails: userContext.hairDetails,
+        allergies: allergies,
+        concerns: concerns,
+      };
+      try {
+        const apiURL =
+          process.env.EXPO_PUBLIC_API_URL +
+          ":5050/client/" +
+          userContext.username;
+        if (!apiURL) {
+          console.error("apiURL not defined");
+          return;
         }
-        setIsEdit(!isEdit);
-    };
+        const res = await axios.put(apiURL, req);
+        console.log("update successful: ", res.data);
+      } catch (error) {
+        console.error("Error with request: ", error);
+      }
 
-    const deleteAccount = async () => {
-        // TODO: update with a separate flow for password validation after backend rework
-
-        try {
-            const apiURL = process.env.EXPO_PUBLIC_API_URL + ':5050/client/' + userContext.username;
-            if (!apiURL) {
-                console.error("apiURL not defined");
-            }
-            const res = axios.delete(apiURL, userContext)
-            console.log(res.message);
-
-        } catch (error) {
-            console.error("Error with request: ", error);
-        }
-        navigation.navigate("SignInPage");
+      // update user context for rest of session
+      userContext = userContext.updateUserContext(req);
     }
+    setIsEdit(!isEdit);
+  };
 
-    const styles = StyleSheet.create({
-        inputContainer: {
-            marginBottom: 10
-        },
-    });
+  const deleteAccount = async () => {
+    // TODO: update with a separate flow for password validation after backend rework
 
-    return (
-        <SignupBackground>
-            <View style={globalStyles.box}>
-                <KeyboardAvoidingInput>
+    try {
+      const apiURL =
+        process.env.EXPO_PUBLIC_API_URL +
+        ":5050/client/" +
+        userContext.username;
+      if (!apiURL) {
+        console.error("apiURL not defined");
+      }
+      const res = axios.delete(apiURL, userContext);
+      console.log(res.message);
+    } catch (error) {
+      console.error("Error with request: ", error);
+    }
+    navigation.navigate("SignInPage");
+  };
 
-                    <View style={styles.inputContainer}>
-                        <Text style={globalStyles.inputHeaderText}>Name</Text>
-                        <TextInput 
-                            style= { globalStyles.input }
-                            placeholder = { userContext.name}
-                            placeholderTextColor={'#000'}
-                            value = { name }
-                            onChangeText={ setName }
-                            editable= { isEdit }
-                        />
-                    </View>
+  const styles = StyleSheet.create({
+    inputContainer: {
+      marginBottom: 10,
+    },
+  });
 
-                    <View style={styles.inputContainer}>
-                        <Text style={globalStyles.inputHeaderText}>Gender</Text>
-                        <TextInput 
-                            style= { globalStyles.input }
-                            placeholder = { userContext.gender}
-                            placeholderTextColor={'#000'}
-                            value = { gender }
-                            onChangeText = { setGender }
-                            editable= { isEdit }
-                        />
-                    </View>
+  return (
+    <SignupBackground>
+      <View style={globalStyles.box}>
+        <KeyboardAvoidingInput>
+          <View style={styles.inputContainer}>
+            <Text style={globalStyles.inputHeaderText}>Name</Text>
+            <TextInput
+              style={globalStyles.input}
+              placeholder={userContext.name}
+              placeholderTextColor={"#000"}
+              value={name}
+              onChangeText={setName}
+              editable={isEdit}
+            />
+          </View>
 
-                    <View style={styles.inputContainer}>
-                        <Text style={globalStyles.inputHeaderText}>Allergies</Text>
-                        <TextInput 
-                            style= { globalStyles.input }
-                            placeholder = { userContext.allergies}
-                            placeholderTextColor={'#000'}
-                            value = { allergies }
-                            onChangeText = { setAllergies }
-                            editable= { isEdit }
-                        />
-                    </View>
+          <View style={styles.inputContainer}>
+            <Text style={globalStyles.inputHeaderText}>Gender</Text>
+            <TextInput
+              style={globalStyles.input}
+              placeholder={userContext.gender}
+              placeholderTextColor={"#000"}
+              value={gender}
+              onChangeText={setGender}
+              editable={isEdit}
+            />
+          </View>
 
-                    <View style={styles.inputContainer}>
-                        <Text style={globalStyles.inputHeaderText}>Concerns</Text>
-                        <TextInput 
-                            style= { globalStyles.input }
-                            placeholder = { userContext.concerns}
-                            placeholderTextColor={'#000'}
-                            value = { concerns }
-                            onChangeText = { setConcerns }
-                            editable= { isEdit }
-                        />
-                    </View>
+          <View style={styles.inputContainer}>
+            <Text style={globalStyles.inputHeaderText}>Allergies</Text>
+            <TextInput
+              style={globalStyles.input}
+              placeholder={userContext.allergies}
+              placeholderTextColor={"#000"}
+              value={allergies}
+              onChangeText={setAllergies}
+              editable={isEdit}
+            />
+          </View>
 
-                </KeyboardAvoidingInput>
+          <View style={styles.inputContainer}>
+            <Text style={globalStyles.inputHeaderText}>Concerns</Text>
+            <TextInput
+              style={globalStyles.input}
+              placeholder={userContext.concerns}
+              placeholderTextColor={"#000"}
+              value={concerns}
+              onChangeText={setConcerns}
+              editable={isEdit}
+            />
+          </View>
+        </KeyboardAvoidingInput>
 
-                <TouchableOpacity style={[globalStyles.button, {marginBottom: 15}]} onPress={handleEdit}>
-                    <Text style={globalStyles.buttonText}>{isEdit ? 'Update' : 'Edit'}</Text>
-                </TouchableOpacity>
+        <TouchableOpacity
+          style={[globalStyles.button, { marginBottom: 15 }]}
+          onPress={handleEdit}
+        >
+          <Text style={globalStyles.buttonText}>
+            {isEdit ? "Update" : "Edit"}
+          </Text>
+        </TouchableOpacity>
 
-                <TouchableOpacity style={globalStyles.button} onPress={deleteAccount}>
-                    <Text style={globalStyles.buttonText}>Delete Account</Text>
-                </TouchableOpacity>
-
-            </View>
-        </SignupBackground>
-    );
-}
+        <TouchableOpacity style={globalStyles.button} onPress={deleteAccount}>
+          <Text style={globalStyles.buttonText}>Delete Account</Text>
+        </TouchableOpacity>
+      </View>
+    </SignupBackground>
+  );
+};
 
 export default ProfileView;
