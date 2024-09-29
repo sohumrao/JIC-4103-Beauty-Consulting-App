df5fc328a12e84e26b0b6237fc6200f2155d7d7e
anand-ts <atsogtjargal3@gatech.edu>
2024-09-29 18:57:49 -0400

    changed stylist details and global styles

--- beauty-consulting-app/app/assets/GlobalStyles.js
+++ beauty-consulting-app/app/assets/GlobalStyles.js
@@ -5,8 +5,8 @@ import {
 const globalStyles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // justifyContent: 'center',
+        // alignItems: 'center',
         backgroundColor: '#fff',
     },
     box: {
