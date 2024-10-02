b329ac5e60632a5e2b3f6e4b0320c08d167022d3
achen443 <achen443@gatech.edu>
2024-10-02 18:08:18 -0400

    Experimental change, signin is stack and navigation in main page is bottomTabNavigator

--- beauty-consulting-app/app/App.js
+++ beauty-consulting-app/app/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LandingPage from './Pages/LandingPage';
 import ClientDetails from './Pages/ClientDetails';
 import ClientDetails2 from './Pages/ClientDetails2';
@@ -17,97 +18,120 @@ import StylistDetails2 from './Pages/StylistDetails2';
 import StylistDetailsComplete from './Pages/StylistDetailsComplete';
 import BusinessInfoPage from './Pages/BusinessInfoPage';
 
-
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const UserContext = React.createContext({
   username: 'not signed in',
-  name: 'No Name', 
+  name: 'No Name',
   age: 0,
   gender: 'male',
   phoneNumber: '0000000000',
   email: 'email@emailsite.com',
-  hairDetails: {Natural: false,
-        Relaxed: false,
-        Straight: false,
-        Wavy: false,
-        Curly: false,
-        DeepWave: false,
-        LooseCurl: false,
-        TightlyCoiled: false,
-        Fine: false,
-        Medium: false,
-        Thick: false},
+  hairDetails: {
+    Natural: false,
+    Relaxed: false,
+    Straight: false,
+    Wavy: false,
+    Curly: false,
+    DeepWave: false,
+    LooseCurl: false,
+    TightlyCoiled: false,
+    Fine: false,
+    Medium: false,
+    Thick: false,
+  },
   allergies: '',
-  concerns: '', 
+  concerns: '',
   stylistDetails: {
     experience: '',
     specialty: '',
     additionalInfo: '',
   },
-  updateUserContext: () => {}
+  updateUserContext: () => {},
 });
 
 export { UserContext };
 
+function ClientDetailsStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="ClientDetails" component={ClientDetails} />
+      <Stack.Screen name="ClientDetails2" component={ClientDetails2} />
+      <Stack.Screen name="ClientDetails3" component={ClientDetails3} />
+      <Stack.Screen name="ClientDetailsComplete" component={ClientDetailsComplete} />
+    </Stack.Navigator>
+  );
+}
+
+function StylistDetailsStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="StylistDetails" component={StylistDetails} />
+      <Stack.Screen name="StylistDetails2" component={StylistDetails2} />
+      <Stack.Screen name="StylistDetailsComplete" component={StylistDetailsComplete} />
+    </Stack.Navigator>
+  );
+}
+
+function MainTabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={LandingPage} />
+      <Tab.Screen name="Client Details" component={ClientDetailsStack} />
+      <Tab.Screen name="Stylist Details" component={StylistDetailsStack} />
+      <Tab.Screen name="Profile" component={ProfilePage} />
+    </Tab.Navigator>
+  );
+}
 
 function App() {
-
-  const updateUserContext = (userContextObject) => {
-    setUserContext(userContextObject);
-  }
-
   const [userContext, setUserContext] = React.useState({
-  name: 'No Name', 
-  age: 0,
-  gender: 'male',
-  phoneNumber: '0000000000',
-  email: 'email@emailsite.com',
-  hairDetails: {Natural: false,
-        Relaxed: false,
-        Straight: false,
-        Wavy: false,
-        Curly: false,
-        DeepWave: false,
-        LooseCurl: false,
-        TightlyCoiled: false,
-        Fine: false,
-        Medium: false,
-        Thick: false},
-  allergies: '',
-  concerns: '', 
-  stylistDetails: {
-    experience: '',
-    specialty: '',
-    additionalInfo: '',
-    businessName: '',
-    businessAddress: ''
-  },
-  updateUserContext
-});
+    name: 'No Name',
+    age: 0,
+    gender: 'male',
+    phoneNumber: '0000000000',
+    email: 'email@emailsite.com',
+    hairDetails: {
+      Natural: false,
+      Relaxed: false,
+      Straight: false,
+      Wavy: false,
+      Curly: false,
+      DeepWave: false,
+      LooseCurl: false,
+      TightlyCoiled: false,
+      Fine: false,
+      Medium: false,
+      Thick: false,
+    },
+    allergies: '',
+    concerns: '',
+    stylistDetails: {
+      experience: '',
+      specialty: '',
+      additionalInfo: '',
+      businessName: '',
+      businessAddress: '',
+    },
+    updateUserContext: (userContextObject) => setUserContext(userContextObject),
+  });
 
   return (
-    
     <UserContext.Provider value={userContext}>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Sign In" component={SignInPage}/>
-        <Stack.Screen name="Create Account" component={CreateAccountPage}/>
-        <Stack.Screen name="LandingPage" component={LandingPage} options={{ title: 'Welcome' }} />
-        <Stack.Screen name="ClientDetails" component={ClientDetails} />
-        <Stack.Screen name="ClientDetails2" component={ClientDetails2} />
-        <Stack.Screen name="ClientDetails3" component={ClientDetails3} />
-        <Stack.Screen name="ClientDetailsComplete" component={ClientDetailsComplete} />
-        <Stack.Screen name="ProfilePage" component={ProfilePage} />
-        <Stack.Screen name="ProfileView" component={ProfileView} />
-        <Stack.Screen name="Forgot Password" component={ForgotPasswordPage}/>
-        <Stack.Screen name="Reset Password" component={ResetPasswordPage}/>
-        <Stack.Screen name="Stylist Details" component={StylistDetails}/>
-        <Stack.Screen name="StylistDetails2" component={StylistDetails2}/>
-        <Stack.Screen name="StylistDetails3" component={StylistDetailsComplete}/>
-        <Stack.Screen name="BusinessInfoPage" component={BusinessInfoPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Sign In" component={SignInPage} screenOptions={{ headerShown: false }}/>
+          <Stack.Screen name="Create Account" component={CreateAccountPage} />
+          <Stack.Screen name="Forgot Password" component={ForgotPasswordPage} />
+          <Stack.Screen name="Reset Password" component={ResetPasswordPage} />
+          <Stack.Screen
+            name="Main"
+            component={MainTabNavigator}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </UserContext.Provider>
   );
 }
