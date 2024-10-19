01ba256b7ccfd66e63e600935fce46728295ca34
David He <davidhe137@gmail.com>
2024-10-19 16:55:25 -0400

    Modify Expo config to allow imports from outside beauty-consulting-app. Create /shared directory to share validation between frontend and backend.

--- beauty-consulting-app/metro.config.js
+++ beauty-consulting-app/metro.config.js
@@ -1,7 +1,16 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(__dirname, "..");
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+	path.resolve(projectRoot, "node_modules"),
+	path.resolve(workspaceRoot, "node_modules"),
+];
 
 module.exports = config;
