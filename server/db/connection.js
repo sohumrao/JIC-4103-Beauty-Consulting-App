4025c3fe831153e6e05f370160e2eac82d774b29
achen443 <143543896+achen443@users.noreply.github.com>
2024-10-22 18:52:44 -0400

    Revert "Added Stylist profile view"

--- server/db/connection.js
+++ server/db/connection.js
@@ -21,14 +21,14 @@ import mongoose from "mongoose";
 
 // Function to establish connection with MongoDB
 const connectDB = async () => {
-    try {
-        // Connecting to MongoDB using provided URI without deprecated options
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected");
-    } catch (err) {
-        console.error("MongoDB connection error:", err);
-        process.exit(1); // Exit process with failure
-    }
+	try {
+		// Connecting to MongoDB using provided URI without deprecated options
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log("MongoDB connected");
+	} catch (err) {
+		console.error("MongoDB connection error:", err);
+		process.exit(1); // Exit process with failure
+	}
 };
 
 export default connectDB;
