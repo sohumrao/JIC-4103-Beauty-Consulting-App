b9b88fc379f9013d384efe3e610d062fa265b69c
willshutt <willmshutt@gmail.com>
2024-10-24 15:06:52 -0400

    make photo not required

--- server/model/account.js
+++ server/model/account.js
@@ -21,11 +21,11 @@ const AccountSchema = new Schema({
 	profilePhoto: {
 		photoData: {
 			type: Buffer, // Storing the image data directly in MongoDB as a binary buffer
-			required: true,
+			required: false,
 		},
 		photoContentType: {
 			type: String, // Storing the file's MIME type
-			required: true,
+			required: false,
 		},
 		uploadedAt: {
 			type: Date,
