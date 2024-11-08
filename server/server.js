d11e43072881396c13aac73d6988458b90586857
David He <davidhe137@gmail.com>
2024-11-08 13:38:59 -0500

    Delete data generation endpoint.

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ import connectDB from "./db/connection.js";
 import stylistController from "./route/StylistController.js";
 import accountController from "./route/AccountController.js";
 import clientController from "./route/ClientController.js";
-import createDataController from "./route/GenerateDataController.js";
 import appointmentController from "./route/AppointmentController.js";
 
 import { errorHandler } from "./errors.js";
@@ -24,7 +23,6 @@ app.use(express.json());
 app.use("/client", clientController);
 app.use("/stylist", stylistController);
 app.use("/account", accountController);
-app.use("/create-data", createDataController);
 app.use("/appointment", appointmentController);
 
 // Throw 404 if no routes were visited
