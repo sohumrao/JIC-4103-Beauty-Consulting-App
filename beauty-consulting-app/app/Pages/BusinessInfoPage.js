96c47acd9e6dff514f1f77d0d456c59cee4c96ac
Anand Tsogtjargal <anandtsogt00@gmail.com>
2024-11-22 18:23:00 -0500

    updated styles for BusinessInfoPage.js, added header to the page

--- beauty-consulting-app/app/Pages/BusinessInfoPage.js
+++ beauty-consulting-app/app/Pages/BusinessInfoPage.js
@@ -1,9 +1,278 @@
+// import React, { useContext, useEffect, useState } from "react";
+// import {
+//   View,
+//   Text,
+//   StyleSheet,
+//   ScrollView,
+//   Dimensions,
+//   Image,
+// } from "react-native";
+// import { UserContext } from "../contexts/userContext";
+// import globalStyles from "../assets/GlobalStyles";
+// import SignupBackground from "../assets/components/SignupBackground";
+// import api from "utils/axios";
+// import { Button } from "react-native-paper";
+// import { SafeAreaView } from "react-native-safe-area-context";
+// import Ionicons from "react-native-vector-icons/Ionicons";
+// import { useNavigation } from "@react-navigation/native";
+// import handleHTTPError from "utils/errorHandling";
+// import { formatDate } from "utils/utils";
+// import StylistServices from "../components/StylistServices";
+// import ImageUploadButton from "../assets/components/ImageUploadButton";
+// import ProfilePhotoDisplay from "../assets/components/ProfilePhotoDisplay";
+
+// const BusinessInfoPage = ({ route }) => {
+//   const { username } = route.params;
+//   const userContext = useContext(UserContext);
+//   const navigation = useNavigation();
+//   const [stylistData, setStylistData] = useState(null);
+//   const [editable, setEditable] = useState(true);
+//   const [photoChanged, setPhotoChanged] = useState(false);
+
+//   useEffect(() => {
+//     setEditable(userContext.username === username);
+//     populateStylistData(username);
+//   }, [username]);
+
+//   const populateStylistData = async (username) => {
+//     try {
+//       const res = await api.get(`/stylist/${username}`);
+//       setStylistData(res.data);
+//     } catch (error) {
+//       handleHTTPError(error);
+//     }
+//   };
+
+//   if (!stylistData) {
+//     return (
+//       <SignupBackground>
+//         <View style={globalStyles.box}>
+//           <Text style={globalStyles.promptText}>Loading...</Text>
+//         </View>
+//       </SignupBackground>
+//     );
+//   }
+
+//   return (
+//     <SignupBackground>
+//       <SafeAreaView style={styles.safeArea}>
+//         <View style={styles.container}>
+//           {/* Header Bar */}
+//           <View style={styles.headerBar}>
+//             {/* Profile Photo */}
+//             {userContext.userProfile?.profilePhoto ? (
+//               <Image
+//                 source={{
+//                   uri: userContext.userProfile.profilePhoto,
+//                 }}
+//                 style={styles.profilePhoto}
+//               />
+//             ) : (
+//               <View style={styles.placeholderPhoto}>
+//                 <Ionicons
+//                   name="person"
+//                   size={24}
+//                   color="#fff"
+//                 />
+//               </View>
+//             )}
+//             {/* Title */}
+//             <Text style={styles.headerTitle}>Business Info</Text>
+//             {/* Placeholder for balancing the header layout */}
+//             <View style={styles.headerRightPlaceholder} />
+//           </View>
+//           {/* End of Header Bar */}
+
+//           {/* Main Content */}
+//           <ScrollView>
+//             <Text style={styles.header}>
+//               {stylistData.business.name}
+//             </Text>
+
+//             <ProfilePhotoDisplay
+//               profilePhoto={stylistData.profilePhoto}
+//               styleProp={styles.photo}
+//             ></ProfilePhotoDisplay>
+
+//             {stylistData.username === userContext.username && (
+//               <ImageUploadButton
+//                 username={userContext.username}
+//                 photoChanged={photoChanged}
+//                 setPhotoChanged={setPhotoChanged}
+//               />
+//             )}
+
+//             <View style={styles.infoContainer}>
+//               <Text style={styles.label}>Stylist Name:</Text>
+//               <Text style={styles.value}>
+//                 {stylistData.info.name}
+//               </Text>
+//             </View>
+
+//             <View style={styles.infoContainer}>
+//               <Text style={styles.label}>Business Address:</Text>
+//               <Text style={styles.value}>
+//                 {stylistData.business.address}
+//               </Text>
+//             </View>
+
+//             <View style={styles.infoContainer}>
+//               <Text style={styles.label}>Birthday:</Text>
+//               <Text style={styles.value}>
+//                 {formatDate(stylistData.info.birthday)}
+//               </Text>
+//             </View>
+
+//             <View style={styles.infoContainer}>
+//               <Text style={styles.label}>Phone Number:</Text>
+//               <Text style={styles.value}>
+//                 {stylistData.info.phoneNumber}
+//               </Text>
+//             </View>
+
+//             <View style={styles.infoContainer}>
+//               <Text style={styles.label}>Email:</Text>
+//               <Text style={styles.value}>
+//                 {stylistData.email}
+//               </Text>
+//             </View>
+
+//             <View style={styles.infoContainer}>
+//               <Text style={styles.label}>
+//                 Years of Experience:
+//               </Text>
+//               <Text style={styles.value}>
+//                 {stylistData.business.experience}
+//               </Text>
+//             </View>
+
+//             <View style={styles.infoContainer}>
+//               <Text style={styles.label}>Specialty:</Text>
+//               <Text style={styles.value}>
+//                 {stylistData.business.specialty}
+//               </Text>
+//             </View>
+
+//             <View style={styles.infoContainer}>
+//               <Text style={styles.label}>Additional Info:</Text>
+//               <Text style={styles.value}>
+//                 {stylistData.business.additionalInfo}
+//               </Text>
+//             </View>
+
+//             <StylistServices
+//               stylistData={stylistData}
+//               setStylistData={setStylistData}
+//               editable={editable}
+//             />
+
+//             {userContext.role === "client" && (
+//               <Button
+//                 style={{ marginBottom: 50 }}
+//                 onPress={() => navigation.goBack()}
+//               >
+//                 Back to Directory
+//               </Button>
+//             )}
+//           </ScrollView>
+//         </View>
+//       </SafeAreaView>
+//     </SignupBackground>
+//   );
+// };
+
+// const screenWidth = Dimensions.get("window").width;
+// const styles = StyleSheet.create({
+//   safeArea: {
+//     flex: 1,
+//   },
+//   container: {
+//     flex: 1,
+//     width: "100%",
+//     paddingHorizontal: 20,
+//   },
+//   headerBar: {
+//     height: 60,
+//     width: "100%",
+//     backgroundColor: "#fa4e41", // Primary color
+//     flexDirection: "row",
+//     alignItems: "center",
+//     justifyContent: "space-between",
+//     paddingHorizontal: 10,
+//     borderRadius: 8,
+//     shadowColor: "#000", // iOS shadow
+//     shadowOffset: { width: 0, height: 2 },
+//     shadowOpacity: 0.2,
+//     shadowRadius: 2,
+//     elevation: 2, // Android shadow
+//     marginBottom: 10, // Space between header and content
+//   },
+//   profilePhoto: {
+//     width: 40,
+//     height: 40,
+//     borderRadius: 20,
+//     backgroundColor: "#fff",
+//   },
+//   placeholderPhoto: {
+//     width: 40,
+//     height: 40,
+//     borderRadius: 20,
+//     backgroundColor: "#555",
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   headerTitle: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//     color: "#fff",
+//   },
+//   headerRightPlaceholder: {
+//     width: 40, // Balances the layout with the profile photo
+//   },
+//   header: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//     marginBottom: 20,
+//     textAlign: "center",
+//   },
+//   infoContainer: {
+//     marginBottom: 15,
+//   },
+//   label: {
+//     fontSize: 18,
+//     fontWeight: "bold",
+//   },
+//   value: {
+//     fontSize: 16,
+//     color: "#555",
+//   },
+//   photo: {
+//     width: screenWidth * 0.5,
+//     height: screenWidth * 0.5, // Ensures height is equal to width
+//     borderRadius: (screenWidth * 0.5) / 2, // Half of the width for a perfect circle
+//     backgroundColor: "#e0e0e0",
+//     alignSelf: "center",
+//   },
+// });
+
+// export default BusinessInfoPage;
+
 import React, { useContext, useEffect, useState } from "react";
-import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	ScrollView,
+	Dimensions,
+	Image,
+} from "react-native";
 import { UserContext } from "../contexts/userContext";
 import globalStyles from "../assets/GlobalStyles";
+import SignupBackground from "../assets/components/SignupBackground";
 import api from "utils/axios";
 import { Button } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import { useNavigation } from "@react-navigation/native";
 import handleHTTPError from "utils/errorHandling";
 import { formatDate } from "utils/utils";
@@ -13,7 +282,6 @@ import ProfilePhotoDisplay from "../assets/components/ProfilePhotoDisplay";
 
 const BusinessInfoPage = ({ route }) => {
 	const { username } = route.params;
-	// Access the user context
 	const userContext = useContext(UserContext);
 	const navigation = useNavigation();
 	const [stylistData, setStylistData] = useState(null);
@@ -21,9 +289,9 @@ const BusinessInfoPage = ({ route }) => {
 	const [photoChanged, setPhotoChanged] = useState(false);
 
 	useEffect(() => {
-		setEditable(userContext.username == username);
+		setEditable(userContext.username === username);
 		populateStylistData(username);
-	});
+	}, [username]);
 
 	const populateStylistData = async (username) => {
 		try {
@@ -36,100 +304,185 @@ const BusinessInfoPage = ({ route }) => {
 
 	if (!stylistData) {
 		return (
-			<View style={globalStyles.box}>
-				<Text style={globalStyles.promptText}>Loading...</Text>
-			</View>
+			<SignupBackground>
+				<View style={globalStyles.box}>
+					<Text style={globalStyles.promptText}>Loading...</Text>
+				</View>
+			</SignupBackground>
 		);
 	}
 
 	return (
-		<ScrollView style={styles.container}>
-			<Text style={styles.header}>{stylistData.business.name}</Text>
+		<SignupBackground>
+			<SafeAreaView style={styles.safeArea}>
+				<View style={styles.container}>
+					{/* Header Bar */}
+					<View style={styles.headerBar}>
+						{/* Profile Photo */}
+						{userContext.userProfile?.profilePhoto ? (
+							<Image
+								source={{
+									uri: userContext.userProfile.profilePhoto,
+								}}
+								style={styles.profilePhoto}
+							/>
+						) : (
+							<View style={styles.placeholderPhoto}>
+								<Ionicons
+									name="person"
+									size={24}
+									color="#fff"
+								/>
+							</View>
+						)}
+						{/* Title */}
+						<Text style={styles.headerTitle}>Business Info</Text>
+						{/* Placeholder for balancing the header layout */}
+						<View style={styles.headerRightPlaceholder} />
+					</View>
+					{/* End of Header Bar */}
 
-			<ProfilePhotoDisplay
-				profilePhoto={stylistData.profilePhoto}
-				styleProp={styles.photo}
-			></ProfilePhotoDisplay>
+					{/* Main Content */}
+					<ScrollView>
+						<Text style={styles.header}>
+							{stylistData.business.name}
+						</Text>
 
-			{stylistData.username == userContext.username && (
-				<ImageUploadButton
-					username={userContext.username}
-					photoChanged={photoChanged}
-					setPhotoChanged={setPhotoChanged}
-				></ImageUploadButton>
-			)}
+						<ProfilePhotoDisplay
+							profilePhoto={stylistData.profilePhoto}
+							styleProp={styles.photo}
+						></ProfilePhotoDisplay>
 
-			<View style={styles.infoContainer}>
-				<Text style={styles.label}>Stylist Name:</Text>
-				<Text style={styles.value}>{stylistData.info.name}</Text>
-			</View>
+						{stylistData.username === userContext.username && (
+							<ImageUploadButton
+								username={userContext.username}
+								photoChanged={photoChanged}
+								setPhotoChanged={setPhotoChanged}
+							/>
+						)}
 
-			<View style={styles.infoContainer}>
-				<Text style={styles.label}>Business Address:</Text>
-				<Text style={styles.value}>{stylistData.business.address}</Text>
-			</View>
+						{/* White Boxes Around Fields */}
+						<View style={styles.whiteBox}>
+							<Text style={styles.label}>Stylist Name:</Text>
+							<Text style={styles.value}>
+								{stylistData.info.name}
+							</Text>
+						</View>
 
-			<View style={styles.infoContainer}>
-				<Text style={styles.label}>Birthday:</Text>
-				<Text style={styles.value}>
-					{formatDate(stylistData.info.birthday)}
-				</Text>
-			</View>
+						<View style={styles.whiteBox}>
+							<Text style={styles.label}>Business Address:</Text>
+							<Text style={styles.value}>
+								{stylistData.business.address}
+							</Text>
+						</View>
 
-			<View style={styles.infoContainer}>
-				<Text style={styles.label}>Phone Number:</Text>
-				<Text style={styles.value}>{stylistData.info.phoneNumber}</Text>
-			</View>
+						<View style={styles.whiteBox}>
+							<Text style={styles.label}>Birthday:</Text>
+							<Text style={styles.value}>
+								{formatDate(stylistData.info.birthday)}
+							</Text>
+						</View>
 
-			<View style={styles.infoContainer}>
-				<Text style={styles.label}>Email:</Text>
-				<Text style={styles.value}>{stylistData.email}</Text>
-			</View>
+						<View style={styles.whiteBox}>
+							<Text style={styles.label}>Phone Number:</Text>
+							<Text style={styles.value}>
+								{stylistData.info.phoneNumber}
+							</Text>
+						</View>
 
-			<View style={styles.infoContainer}>
-				<Text style={styles.label}>Years of Experience:</Text>
-				<Text style={styles.value}>
-					{stylistData.business.experience}
-				</Text>
-			</View>
+						<View style={styles.whiteBox}>
+							<Text style={styles.label}>
+								Years of Experience:
+							</Text>
+							<Text style={styles.value}>
+								{stylistData.business.experience}
+							</Text>
+						</View>
 
-			<View style={styles.infoContainer}>
-				<Text style={styles.label}>Specialty:</Text>
-				<Text style={styles.value}>
-					{stylistData.business.specialty}
-				</Text>
-			</View>
+						<View style={styles.whiteBox}>
+							<Text style={styles.label}>Specialty:</Text>
+							<Text style={styles.value}>
+								{stylistData.business.specialty}
+							</Text>
+						</View>
 
-			<View style={styles.infoContainer}>
-				<Text style={styles.label}>Additional Info:</Text>
-				<Text style={styles.value}>
-					{stylistData.business.additionalInfo}
-				</Text>
-			</View>
-			<StylistServices
-				stylistData={stylistData}
-				setStylistData={setStylistData}
-				editable={editable}
-			/>
-			{userContext.role === "client" ? (
-				<Button
-					style={{ marginBottom: 50 }}
-					onPress={() => navigation.goBack()}
-				>
-					Back to Directory
-				</Button>
-			) : null}
-		</ScrollView>
+						<View style={styles.whiteBox}>
+							<Text style={styles.label}>Additional Info:</Text>
+							<Text style={styles.value}>
+								{stylistData.business.additionalInfo}
+							</Text>
+						</View>
+
+						<View style={styles.whiteBox}>
+							<StylistServices
+								stylistData={stylistData}
+								setStylistData={setStylistData}
+								editable={editable}
+							/>
+						</View>
+
+						{userContext.role === "client" && (
+							<Button
+								style={{ marginBottom: 50 }}
+								onPress={() => navigation.goBack()}
+							>
+								Back to Directory
+							</Button>
+						)}
+					</ScrollView>
+				</View>
+			</SafeAreaView>
+		</SignupBackground>
 	);
 };
 
 const screenWidth = Dimensions.get("window").width;
 const styles = StyleSheet.create({
+	safeArea: {
+		flex: 1,
+	},
 	container: {
 		flex: 1,
-		padding: 20,
+		width: "100%",
+		paddingHorizontal: 20,
+	},
+	headerBar: {
+		height: 60,
+		width: "100%",
+		backgroundColor: "#fa4e41",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		paddingHorizontal: 10,
+		borderRadius: 8,
+		shadowColor: "#000",
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.2,
+		shadowRadius: 2,
+		elevation: 2,
+		marginBottom: 10,
+	},
+	profilePhoto: {
+		width: 40,
+		height: 40,
+		borderRadius: 20,
 		backgroundColor: "#fff",
-		marginTop: 20, // temp fix to make more readable, TODO:
+	},
+	placeholderPhoto: {
+		width: 40,
+		height: 40,
+		borderRadius: 20,
+		backgroundColor: "#555",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	headerTitle: {
+		fontSize: 20,
+		fontWeight: "bold",
+		color: "#fff",
+	},
+	headerRightPlaceholder: {
+		width: 40,
 	},
 	header: {
 		fontSize: 24,
@@ -137,8 +490,16 @@ const styles = StyleSheet.create({
 		marginBottom: 20,
 		textAlign: "center",
 	},
-	infoContainer: {
-		marginBottom: 15,
+	whiteBox: {
+		backgroundColor: "#fff",
+		padding: 15,
+		marginVertical: 10,
+		borderRadius: 8,
+		shadowColor: "#000",
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.1,
+		shadowRadius: 4,
+		elevation: 2,
 	},
 	label: {
 		fontSize: 18,
@@ -150,8 +511,8 @@ const styles = StyleSheet.create({
 	},
 	photo: {
 		width: screenWidth * 0.5,
-		height: screenWidth * 0.5, // Ensures height is equal to width
-		borderRadius: (screenWidth * 0.5) / 2, // Half of the width for a perfect circle
+		height: screenWidth * 0.5,
+		borderRadius: (screenWidth * 0.5) / 2,
 		backgroundColor: "#e0e0e0",
 		alignSelf: "center",
 	},
