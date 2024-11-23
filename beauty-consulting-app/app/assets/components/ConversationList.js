b3b72377d5445445d97a077000fa75c7cba2d263
achen443 <achen443@gatech.edu>
2024-11-23 18:32:46 -0500

    Added useEffect to populate conversations page

---
+++ beauty-consulting-app/app/assets/components/ConversationList.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { FlatList, StyleSheet, View, Text } from "react-native";
+import ConversationItem from "./ConversationItem";
+
+const ConversationsList = ({ conversations }) => {
+	const renderItem = ({ item }) => (
+		<ConversationItem
+			name={item.name}
+			messagePreview={item.messagePreview}
+			timestamp={item.timestamp}
+		/>
+	);
+
+	return (
+		<View style={styles.container}>
+			{conversations && conversations.length > 0 ? (
+				<FlatList
+					data={conversations}
+					renderItem={renderItem}
+					keyExtractor={(item, index) => index.toString()}
+				/>
+			) : (
+				<Text style={styles.noMessagesText}>
+					No recent conversations.
+				</Text>
+			)}
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#f9f9f9",
+	},
+	noMessagesText: {
+		marginTop: 20,
+		textAlign: "center",
+		color: "#666",
+		fontSize: 16,
+	},
+});
+
+export default ConversationsList;
